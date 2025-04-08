2b55882eff238e6cf2f51ef0c736980cad59c56d
Simon Pokorny <git@simon-pokorny.com>
2025-04-08 21:18:59 +0200

    Update form_template.php

--- form_template.php
+++ form_template.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Sitemap Validator - Überprüfen Sie Ihre sitemap.xml auf Fehler und Konformität mit den Standards">
-    <title>Sitemap Validator</title>
+    <title>XML Sitemap Validator</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
 </head>
